5a460acf7bc6a7a04234d618afd2a1070a136ab8
Dosilly <dosilly.darek@gmail.com>
2018-10-15 18:19:28 +0200

    Initial commit

---
+++ src/app/usersTable/usersTable.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+    selector: 'app-userstable',
+    templateUrl: './usersTable.component.html'
+})
+
+export class UsersTableComponent {
+
+}
